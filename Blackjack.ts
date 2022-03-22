9b6123681ace35f36cf76d0dae4208a9e5962091
vrecek <oktawiusz2002@gmail.com>
2022-03-22 17:49:48 +0100

    Initial commit

---
+++ Blackjack.ts
@@ -0,0 +1,176 @@
+export default class Blackjack {
+   private card_images:Array<string>
+   private enemy_score:number
+   private enemy_hold:boolean
+   private player_score:number
+   private player_hold:boolean
+   private player_money:number
+
+   /* ----------------------------------------------------------------------------- */
+
+   private cardScore(num:number, arr:Array<number>, turn:string) {
+      switch(num) {
+         case 0: case 1: case 2: 
+         case 3: case 4: case 5: 
+         case 6: case 7: case 8:
+            arr.push(num + 2)
+         break;
+
+         case 9: case 10: case 11:
+            arr.push(10)
+         break;
+
+         case 12:
+            if(turn === 'player') {
+               arr.push( this.player_score + 11 > 21 ? 1 : 11 )
+               break;
+            }
+
+            arr.push( this.enemy_score + 11 > 21 ? 1 : 11 )
+            break;
+      }
+
+      return arr
+   }
+
+   private enemyHold(nextNum:number):boolean {
+      if(this.enemy_hold) return true
+
+      const arr = this.cardScore(nextNum, [], 'enemy')
+
+      const more21 = this.enemy_score + arr[0] > 21
+
+      if(this.player_hold && this.enemy_score > this.player_score) return true
+
+      if(more21 && this.player_score <= this.enemy_score) {
+         const over = 21 - this.enemy_score
+
+         const rand = Math.floor(Math.random() * 100) + 1
+
+         if(rand <= over) {
+            return false
+         }
+
+         this.enemy_hold = true
+         return true
+      }
+
+      return false
+   }
+
+   /* ----------------------------------------------------------------------------- */
+
+   public constructor(card_images:Array<string>, starter_plr_money:number) {
+      this.player_score = 0
+      this.enemy_score = 0
+      this.enemy_hold = false
+      this.card_images = card_images
+      this.player_money = starter_plr_money
+      this.player_hold = false
+   }
+
+   public draw(player_and_enemy_fields:Array<HTMLElement | Element>, enemyHoldText?:HTMLElement | Element):Array<number> {
+      if(player_and_enemy_fields.length !== 2) throw new Error('Fields must be for a player and enemy')
+
+      const rands:Array<number> = [...Array(2)].map(x => Math.floor(Math.random() * this.card_images.length))
+      const score:Array<number> = []
+      const turnarray:Array<any> = [...player_and_enemy_fields]
+
+      const enemy_hold = this.enemyHold(rands[1])
+      if(enemy_hold) {
+         turnarray.splice(1, 1)
+         enemyHoldText ? enemyHoldText.textContent = '(hold)': null
+      }
+
+      for( let [i, x] of Object.entries(turnarray) ) {
+         const rand = rands[parseInt(i)]
+
+         let turn:string = 'enemy'
+         if(turnarray.length === 2) {
+            turn = parseInt(i) === 0 ? 'player' : 'enemy'
+         } 
+
+         const img = document.createElement('img')
+         img.src = this.card_images[rand]
+
+         this.cardScore(rand, score, turn)
+
+         x.appendChild(img)
+      }
+
+      this.player_score += score[0]
+      this.enemy_score += score[1]
+
+      return score
+   }
+
+   public returnWinner():string {
+      if(this.player_score > 21 || this.enemy_score > 21) {
+         return this.player_score > this.enemy_score ? 'enemy' : 'player'
+      }
+
+      if(
+         (this.player_score === this.enemy_score && this.player_hold && this.enemy_hold) ||
+         this.player_score === 21 && this.enemy_score === 21
+      ) {
+         return 'DRAW'
+      }
+
+      if(
+         (this.player_score === 21 || this.enemy_score > 21) ||
+         (this.enemy_hold && this.player_score > this.enemy_score)
+      ) {
+         return 'player'
+      }
+
+      if(
+         (this.enemy_score === 21 || this.player_score > 21) ||
+         (this.player_hold && this.enemy_score > this.player_score)
+      ) {
+         return 'enemy'
+      }
+
+      return ''
+   }
+
+   public hold(enemy_board:HTMLElement | Element, enemy_score:HTMLElement | Element) {
+      this.player_hold = true
+
+      while(!this.returnWinner()) {
+         const rand = Math.floor(Math.random() * this.card_images.length)
+
+         if(this.enemyHold(rand)) {
+            break
+         }
+
+         const score = this.cardScore(rand, [], 'enemy')
+
+         const img = document.createElement('img')
+         img.src = this.card_images[rand]
+
+         enemy_board.appendChild(img)
+         this.enemy_score += score[0]
+         enemy_score.textContent = (parseInt(enemy_score.textContent!) + score[0]).toString()
+      }
+   }
+
+   public finish() {
+
+   }
+
+   public restartGame(player_and_enemy_fields:Array<HTMLElement | Element>, scores_cont:Array<HTMLElement | Element>) {
+      if(player_and_enemy_fields.length !== 2) throw new Error('Fields must be for a player and enemy')
+
+      this.player_score = 0
+      this.enemy_score = 0
+      this.enemy_hold = false
+      this.player_hold = false
+
+      for(let i = 0; i <= 1; i++) {
+         scores_cont[i].textContent = '0'
+
+         for( let x of Array.from(player_and_enemy_fields[i].children) ) x.remove()
+      }
+   }
+
+}
---
+++ index.ts
@@ -0,0 +1,95 @@
+import Blackjack from "./Blackjack.js"
+
+const card_images:Array<string> = [
+   './images/card-2.png',
+   './images/card-3.png',
+   './images/card-4.png',
+   './images/card-5.png',
+   './images/card-6.png',
+   './images/card-7.png',
+   './images/card-8.png',
+   './images/card-9.png',
+   './images/card-10.png',
+   './images/card-jack.png',
+   './images/card-queen.png',
+   './images/card-king.png',
+   './images/card-ace.png',
+]
+
+const draw_card = document.querySelector('.draw-card') as HTMLElement
+const hold_card = document.querySelector('.hold-card') as HTMLElement
+const restart = document.querySelector('.restart') as HTMLElement
+
+const win_cont = document.querySelector('h4') as HTMLElement
+
+const ene_hold_cont = document.querySelector('.enemy h2 span') as HTMLElement
+const plr_hold_cont = document.querySelector('.player h2 span') as HTMLElement
+
+/* player [0], enemy [1] */
+const scores_cont:Array<any> = Array.from(document.querySelectorAll('.score'))
+const fields = Array.from(document.querySelectorAll('.cards'))
+/* */
+
+const game = new Blackjack(card_images, 500)
+
+draw_card.addEventListener('click', () => {
+   const turn_score = game.draw(fields, ene_hold_cont)
+
+   for(let i = 0; i <= 1; i++) {
+      scores_cont[i].textContent = parseInt(scores_cont[i].textContent) + (turn_score[i] || 0)
+   }
+
+   const winner = game.returnWinner()
+
+   if(winner) {
+      end(winner)
+   }
+})
+
+hold_card.addEventListener('click', () => {
+   plr_hold_cont.textContent = '(hold)'
+
+   game.hold(fields[1], scores_cont[1])
+   const result = game.returnWinner()
+
+   end(result)
+})
+
+restart.addEventListener('click', () => {
+   game.restartGame(fields, scores_cont)
+
+   win_cont.style.display = 'none'
+
+   draw_card.style.pointerEvents = 'all'
+   draw_card.style.visibility = 'visible'
+
+   hold_card.style.pointerEvents = 'all'
+   hold_card.style.visibility = 'visible'
+
+   restart.style.display = 'none'
+
+   ene_hold_cont.textContent = ''
+   plr_hold_cont.textContent = ''
+
+   win_cont.children[1].textContent = ''
+   win_cont.children[1].textContent = 'WINS'
+})
+
+const end = (winnerString:string) => {
+   win_cont.children[1].textContent = winnerString
+
+   if(winnerString === 'DRAW') {
+      win_cont.children[1].textContent = ''
+   }
+
+   win_cont.style.display = 'block'
+
+   draw_card.style.pointerEvents = 'none'
+   draw_card.style.visibility = 'hidden'
+
+   hold_card.style.pointerEvents = 'none'
+   hold_card.style.visibility = 'hidden'
+
+   restart.style.display = 'inline'
+}
+
